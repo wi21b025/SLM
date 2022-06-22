62a57bcba3829c01a8a9fb94d02d1f146cf951f2
wi21b025 <wi21b025@technikum-wien.at>
2022-06-22 13:44:34 +0200

    Initial commit

---
+++ src/main/java/com/example/slm/SlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slm/SlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
