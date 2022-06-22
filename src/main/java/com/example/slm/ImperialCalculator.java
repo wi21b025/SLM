cf98192823911bfd4e67290335d1ea347cddd21c
wi21b025 <wi21b025@technikum-wien.at>
2022-06-22 13:53:56 +0200

    commit develop

---
+++ src/main/java/com/example/slm/ImperialCalculator.java
@@ -0,0 +1,5 @@
+package com.example.slm;
+
+public class ImperialCalculator {
+
+}
---
+++ src/main/java/com/example/slm/ImperialCalculatorController.java
@@ -0,0 +1,14 @@
+package com.example.slm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImperialCalculatorController {
+    ImperialCalculator e = new ImperialCalculator();
+
+
+
+
+}
